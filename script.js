aee3a4fc49b656f912efe264339178043083eef8
Tomazzyy <129331153+Tomazzyy@users.noreply.github.com>
2024-09-04 10:06:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function calcularr(){
+    let real = document.getElementById("valorreal").value
+    let dolar = document.getElementById("valordolar").value
+
+    let resultado = real / dolar
+    alert(resultado)
+
+}
